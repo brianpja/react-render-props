63ef071546e6749153ad765933afdc39a5a82299
Brian James <brian.james@slalom.com>
2019-03-08 10:03:11 -0800

    added example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import MouseTracker from './MouseTracker';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Render Props Examples
+        Render Props Example
+        <MouseTracker />
       </div>
     );
   }
